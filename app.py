8839b78e85b72c1a6ace11a2cf92eed04fe87d28
Mina Fakhry <82112956+MInaAzer01@users.noreply.github.com>
2022-10-15 15:39:29 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug import secure_filename
+import os
+import numpy as np
+import struct
+import matplotlib.pyplot as plt
+import math
+import wave
+from scipy.misc import electrocardiogram
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecret'
+app.config['UPLOAD_FOLDER'] = 'static/files'
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Upload File")
 
+@app.route('/', methods=['GET',"POST"])
+@app.route('/index/', methods=['GET',"POST"])
+def home():
+    form = UploadFileForm()
+    return render_template('index.html', form=form)
 
 if __name__ =="__main__":
     app.run(debug=True)
